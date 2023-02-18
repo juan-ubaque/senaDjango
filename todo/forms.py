413a0cb2ccf81678b0f9b9fe60d942a1cdfb51d3
Juan DAvid <113227362+juan-ubaque@users.noreply.github.com>
2023-02-18 12:12:44 -0500

    proyecto con estilos

--- todo/forms.py
+++ todo/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from . models import Tarea
 
 class TareaForm (forms.ModelForm):
-    
+    titulo = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    descripcion = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    estado = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), required=False)
     class Meta: 
         model = Tarea
         fields = ['titulo', 'descripcion', 'estado']
